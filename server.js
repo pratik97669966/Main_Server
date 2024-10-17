5c3c69a9792e12d4c47ab7d43cfe3908aaa8dad1
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2024-10-17 18:57:35 +0530

    ll

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const GOOGLE_APPS_SCRIPT_URL = process.env.GOOGLE_APPS_SCRIPT_URL || 'https://script.google.com/macros/s/.../exec';
+const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwcWqDV-pNAoBQkeUWPXQUrq6BdORvr5zeoiBz4lZurHnooOn_POC6HFloYk4p5p6_W/exec';
 
 // Define your proxy endpoint for POST (Add Entry)
 app.post('/data', async (req, res) => {

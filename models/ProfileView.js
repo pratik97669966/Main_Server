13a1f0be414e6d7377222b4258ef1c00d5d77ae3
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2024-11-15 18:33:05 +0530

    hh

--- models/ProfileView.js
+++ models/ProfileView.js
@@ -1,13 +1,7 @@
-// models/ProfileView.js
 const mongoose = require('mongoose');
-
 const ProfileViewSchema = new mongoose.Schema({
-    viewerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    viewedUserId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    viewedAt: { type: Date, default: Date.now }
+    viewerId: { type: String, required: true },
+    viewedUserId: { type: String, required: true },
+    date: { type: Date, default: Date.now }
 });
-
-// Indexing to speed up queries based on viewer and viewed users
-ProfileViewSchema.index({ viewerId: 1, viewedUserId: 1 });
-
 module.exports = mongoose.model('ProfileView', ProfileViewSchema);

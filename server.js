f9dc6f67fa1afaf18b4435944e1c5b068115db3b
TechByAtharv <officialopentalk@gmail.com>
2025-02-18 20:57:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true,
           user.id = socket.id;
           usersCollection.insertOne(user)
             .then(() => {
-              console.log(`User ${user.name} added to MongoDB`);
+              console.log(`User ${user.username} added to MongoDB`);
               io.emit('user-add', user);
             })
             .catch((err) => {

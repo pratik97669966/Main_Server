36db297d68fb82ff6957afb0629396d85d3a9a56
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2025-03-25 10:02:26 +0530

    ll

--- controllers/userController.js
+++ controllers/userController.js
@@ -156,12 +156,13 @@ exports.iwant = async (req, res) => {
             if (business) {
                 const businessRecord = await IWantBusiness.findOne({ businessNumber: business.businessNumber });
 
-                if (businessRecord && !requestNote.includes("I am interested for ")) {
-                    const thirtyMinutesAgo = new Date(Date.now() - 1 * 60 * 1000);
+                if (businessRecord) {
+                    const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000);
                     const recentCustomerRecord = await IWantBusiness.findOne({
                         businessNumber: business.businessNumber,
                         'customerList.customerMobile': customerMobile,
-                        'customerList.date': { $gte: thirtyMinutesAgo }
+                        // 'customerList.date': { $gte: thirtyMinutesAgo },
+                        'customerList.requestNote': requestNote
                     }, {
                         'customerList.$': 1
                     });
